7cf200ffadd2589aa5b8f2e68e3f90dd88f1ecfe
Victor Grando <victorgrando09@gmail.com>
2025-02-17 18:57:48 -0300

    Saving user's infomation in session

--- librarygames.py
+++ librarygames.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask,render_template,request,redirect,session, flash
 class Game:
         def __init__(self, name, category, console):
                 self.name = name
@@ -17,6 +17,7 @@ listgames = [game1 , game2]
 
 
 app = Flask(__name__)
+app.secret_key = 'ONE PIECE > NARUTO' #key to encrypt session data
 
 '''telling Flask that when someone accesses the *home* URL, the hello()
         function should be called, and what this function returns will be displayed in the browser
@@ -38,19 +39,30 @@ def new_games():
 def login():
         return render_template('login.html', title = 'Login')
 
-#AUTHENTICATION
-@app.route('/authenticate', methods = ['POST', ])
+
+@app.route('/authenticate', methods = ['POST', ]) #AUTHENTICATION
 def authenticate():
         user = request.form['user']
         password = request.form['password']
         if user == 'admin' and password == 'admin':
+                session['user_logged'] =request.form['user'] #which information I wanna store in session
+                
+                flash(session['user_logged'] + 'logged successfully')
+                ''' FLASH is a function 
+                that shows a  fast message into users screen
+                simmilar to print, but pertences to Flask
+                BUT TO MAKE THE  MESSAGE APPEARS I NEED A FLASH MESSAGE BLOCK, WHICH NEEDS TO BE PLACED IN  HTML CODE
+                
+                If I don't store the information in the session when the user has logged, the success message will still be displayed, 
+                but you won't have access to the user's name in other parts of the application, 
+                which can be a problem I you want to personalize the user's experience or check whether the user is logged in.'''
                 return redirect('/newgames')
         else:
+                flash('Invalid user or password, try again')
                 return redirect('/login')
 
 
-#CREATING NEW GAMES
-@app.route('/cratinnewgames', methods = ['POST', ])
+@app.route('/cratinnewgames', methods = ['POST', ])#CREATING NEW GAMES
 def creatin_new_games():
         
         name = request.form['name']
@@ -67,4 +79,5 @@ def creatin_new_games():
 
 
 app.run(debug=True)
-#app.run(host='0.0.0.0', port=8080) |When I wanna  allow external access to the application I can use the HOST 0.0.0.0
+'''app.run(host='0.0.0.0', port=8080) |When I wanna  allow external access to the
+ application I can use the HOST 0.0.0.0'''
